ad1c58355f916a5129a1d921fa3e0be18b22887e
Alex Proskuriakov <alexpro1972@yandex.ru>
2023-07-08 00:20:21 +0300

    Added test core

--- tests/test_core.py
+++ tests/test_core.py
@@ -2,12 +2,10 @@ from typing import AsyncGenerator
 
 import pytest
 
-from sqlalchemy.ext.asyncio import AsyncSession
-
 from .conftest import get_async_session
 
 
 @pytest.mark.anyio
 async def test_get_async_session():
-    isinstance(get_async_session, AsyncSession)
-    isinstance(get_async_session(), AsyncGenerator)
+    assert isinstance(get_async_session(), AsyncGenerator)
+
