5343ef887aeb56139aab1402520023a89397e322
Alex Proskuriakov <alexpro1972@yandex.ru>
2023-07-07 13:20:13 +0300

    Added some crud tests

--- tests/test_crud.py
+++ tests/test_crud.py
@@ -1,9 +1,15 @@
 import pytest
 
-def test_not_implemented(get_crud_base):
+from app.models import User
+
+
+@pytest.mark.parametrize('method, args', (
+    ('has_permission', (None, None)),
+    ('is_update_allowed', (None, None)),
+    ('is_delete_allowed', (None,)),
+    ('perform_create', (None, None)),
+    ('perform_update', (None, None)),
+))
+def test_not_implemented(get_crud_base, method, args):
     with pytest.raises(NotImplementedError):
-        get_crud_base.has_permission(None, None)
-        get_crud_base.is_update_allowed(None, None)
-        get_crud_base.is_delete_allowed(None, None)
-        get_crud_base.perform_create(None, None)
-        get_crud_base.perform_update(None, None)
+        get_crud_base.__getattribute__(method)(*args)
