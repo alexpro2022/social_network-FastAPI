c66b4851dd3dc280863e539de34224b6e8402efe
Alex Proskuriakov <alexpro1972@yandex.ru>
2023-07-07 14:33:20 +0300

    Refactoring crud tests

--- tests/test_crud.py
+++ tests/test_crud.py
@@ -1,24 +1,26 @@
+from http import HTTPStatus
 import pytest
-import re
 
 from fastapi import HTTPException
 
-from app.models import User
 
-
-@pytest.mark.parametrize('method, args, msg', (
+@pytest.mark.parametrize('method, args, expected_msg', (
     ('has_permission', (None, None), 'has_permission() must be implemented.'),
     ('is_update_allowed', (None, None), 'is_update_allowed() must be implemented.'),
     ('is_delete_allowed', (None,), 'is_delete_allowed() must be implemented.'),
     ('perform_create', (None, None), 'perform_create() must be implemented.'),
     ('perform_update', (None, None), 'perform_update() must be implemented.'),
 ))
-def test_not_implemented_exception(get_crud_base, method, args, msg):
-    with pytest.raises(NotImplementedError, match=re.escape(msg)):
+def test_not_implemented_exception(get_crud_base, method, args, expected_msg):
+    with pytest.raises(NotImplementedError) as exc_info:
         get_crud_base.__getattribute__(method)(*args)
+    assert exc_info.value.args[0] == expected_msg
 
 
 @pytest.mark.parametrize('method', ('get_all_by_attr', 'get_by_attr'))
 async def test_not_found_exception(get_crud_base, get_test_session, method):
-    with pytest.raises(HTTPException, match=re.escape('Object(s) not found.')):
+    expected_msg = 'Object(s) not found.'
+    with pytest.raises(HTTPException) as exc_info:
         await get_crud_base.__getattribute__(method)(get_test_session, 'title', None, True)
+    assert exc_info.value.args[0] == HTTPStatus.NOT_FOUND
+    assert exc_info.value.args[1] == expected_msg
